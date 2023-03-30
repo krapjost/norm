4d6b98df453b343899f71aa7eec76fbf70e31905
krapjost <krapjost@gmail.com>
2023-03-31 02:35:01 +0900

    Modify null-ls

--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -3,10 +3,14 @@ return function()
 	null_ls.setup({
 		sources = {
 			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.dprint,
+			null_ls.builtins.diagnostics.eslint,
+			null_ls.builtins.formatting.prettier,
 			null_ls.builtins.completion.spell,
 		},
 		on_attach = function(client, bufnr)
+			if client.name ~= "null-ls" then
+				return
+			end
 			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 			if client.supports_method("textDocument/formatting") then
 				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -14,12 +18,7 @@ return function()
 					group = augroup,
 					buffer = bufnr,
 					callback = function()
-						vim.lsp.buf.format({
-							bufnr = bufnr,
-							filter = function(_client)
-								return _client.name == "null-ls"
-							end,
-						})
+						vim.lsp.buf.format()
 					end,
 				})
 			end
